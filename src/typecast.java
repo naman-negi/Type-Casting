37f56fae1ac5788246a2925e87176fd397c02194
Naman <namanv2@gmail.com>
2021-06-24 15:01:30 +0530

    First Commit

---
+++ src/typecast.java
@@ -0,0 +1,25 @@
+
+public class typecast {
+
+	public int stringconvert(String y, int raddix) {
+		
+		int x = Integer.parseInt(y,raddix);
+		
+	}
+	public long dblconvert(long y) {
+		
+		long x = (long) y;
+		
+	}
+	public float dblconvert2(double y) {
+		
+		float x = (float) y;
+		
+	}
+	
+	public char intconvert(int y) {
+		
+		char x = 
+	}
+	
+}
