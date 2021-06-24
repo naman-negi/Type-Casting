68c3c35c25323335afb1b252c8b3e3c784a5a7ee
Naman <namanv2@gmail.com>
2021-06-24 15:25:21 +0530

    Third Commit

---
+++ mainfunction.java
@@ -0,0 +1,32 @@
+
+public class mainfunction {
+
+	public static void main() {
+		
+		int x1=65;
+		double x2=1.12345678910;
+		long x3;
+		String x4="444";
+		int raddix =16; 
+		
+		typecast calling = new typecast ;
+		
+		System.out.println("Original input in int:"+x1);
+		System.out.println("Converted Output to char:"+calling.intconvert(x1)); 
+		System.out.println("\n");
+		
+		System.out.println("Original input in double:"+x2);
+		System.out.println("Converted Output to long:"+calling.dblconvert(x2)); 
+		System.out.println("\n");
+
+		System.out.println("Original input in String (which is in hexadecimal form):"+x4);
+		System.out.println("Converted Output to int:"+calling.stringconvert(x4,raddix)); 
+		System.out.println("\n");
+		
+		System.out.println("Original input in double:"+x2);
+		System.out.println("Converted Output to float:"+calling.dblconvert2(x2)); 
+		System.out.println("\n");
+		
+		
+		}
+}
--- src/typecast.java
+++ src/typecast.java
@@ -4,22 +4,23 @@ public class typecast {
 	public int stringconvert(String y, int raddix) {
 		
 		int x = Integer.parseInt(y,raddix);
-		
+		return x;
 	}
-	public long dblconvert(long y) {
+	public long dblconvert(double y) {
 		
 		long x = (long) y;
-		
+		return x
 	}
 	public float dblconvert2(double y) {
 		
 		float x = (float) y;
-		
+		return x;
 	}
 	
 	public char intconvert(int y) {
 		
 		char x = (cha) y;
+		return x;
 	}
 	
 }
